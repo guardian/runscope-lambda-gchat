a2512dc39da94bf1c656d2bbb5e0f64bbf5aae72
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-07-21 17:28:17 +0100

    Working version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
- 
+
 const axios = require('axios');
 
 exports.handler = async (event) => {
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
     gchatUrl = event.headers.gchaturl
   } else {
     console.log("no gchat url provided");
-  }  
+  }
 
   //get url for agent failure gchat channel from header
   let hasAgentChannel = false;
@@ -24,33 +24,9 @@ exports.handler = async (event) => {
     hasAgentChannel = true;
   } else {
     console.log("no gchat url provided for runscope agent");
-  }   
-
-     
-  //deconstruct variables
-  const {bucket_name, test_name, test_url, test_run_url, result, environment_name, region, region_name, requests, variables, agent, agent_expired} = webhookData;
-
-  //allow alternate test name under custom_name test variable and custom message under custom_message test variable
-  let my_test_name = test_name;
-  let my_custom_message = "";
-  let isCustomMessage = false;
-  let extraGchatMessage = {};
-  if(variables){
-    if (variables.custom_name) {
-        my_test_name = variables.custom_name;
-    }
-  
-    if (variables.custom_message) {
-      my_custom_message = variables.custom_message;
-      isCustomMessage = true;
-      extraGchatMessage = {
-        "title": "Message",
-        "value": my_custom_message,
-        "short": true
-      };
-    }
   }
-  //define variables for display
+
+  const { test_name, test_url, test_run_url, result, environment_name, region, region_name, requests, variables, agent, agent_expired } = webhookData;
   let totalResponseTime = 0;
   let assertionsTotal = 0;
   let assertionsPassed = 0;
@@ -58,118 +34,123 @@ exports.handler = async (event) => {
   let scriptsPassed = 0;
   let requestCount = requests.length;
   let thisResult = result;
-  let location;
-  if (region) {
-    let region_name_string = region_name.split(" - ",1);
-    location = `${region.toUpperCase()}: ${region_name_string}`;
-  } else {
-    location = agent;
-  }
-  for (let i=0; i<requests.length; i++) {
+  let my_region_name = region_name.split("-", 1);
+  for (let i = 0; i < requests.length; i++) {
     assertionsTotal += requests[i].assertions.total;
     assertionsPassed += requests[i].assertions.pass;
     scriptsTotal += requests[i]["scripts"].total;
     scriptsPassed += requests[i]["scripts"].pass;
     totalResponseTime += requests[i].response_time_ms;
   }
-  
-  let blue = "#0000ff";
-  let red = "#ff0000";
-  let green = "#2ecc71";
+  let thisIcon = "https://www.runscope.com/static/img/press/logoicon-runscope-color.png";
+  const failIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Antu_task-reject.svg/200px-Antu_task-reject.svg.png";
+  const passIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Antu_vcs-normal.svg/200px-Antu_vcs-normal.svg.png";
+  let blue = "#0000ff" // blue    
+  let red = "#ed0000"; //"#ff0000";    
+  let green = "#2ecc71";// "#00ff00";    
   let fontColor = blue;
   if (result == "pass") {
+    thisIcon = passIcon;
     thisResult = "Passed";
     fontColor = green;
   } else if (result == "fail") {
+    thisIcon = failIcon;
     thisResult = "Failed";
     fontColor = red;
   }
-
-  //create payload for gchat message
-  let html_message = `API test run completed. <${test_run_url}|View Result> - <${test_url}|Edit Test>`;
-  let plain_text_message = `${bucket_name}: ${my_test_name} test run ${result} in ${location}. ${requests.length} executed, ${assertionsPassed} of ${assertionsTotal} assertions passed.`;
-
-  let gchatdata =  [
-    {
-      "title": `${bucket_name}: ${my_test_name}`,
-      "title_link": test_url,
-      "pretext": html_message,
-      "fallback": plain_text_message,
-      "color": fontColor,
-      "fields": [
-        {
-            "title": "Status",
-            "value": thisResult,
-            "short": true
+  const cards = [{
+    "header": {
+      "title": "APITestRunCompleted",
+      "subtitle": test_name,
+      "imageUrl": thisIcon,
+      "imageStyle": "IMAGE"
+    },
+    "sections": [
+      {
+        "widgets": [
+          {
+            "buttons": [
+              { "textButton": { "text": "ViewResult", "onClick": { "openLink": { "url": test_run_url } } } },
+              { "textButton": { "text": "EditTest", "onClick": { "openLink": { "url": test_url } } } }
+            ]
+          }
+        ]
+      },
+      {
+        "widgets": [{
+          "keyValue": {
+            "topLabel": "Status",
+            "content": `<font color=\"${fontColor}\">${thisResult}</font>`
+          }
+        }, {
+          "keyValue": {
+            "topLabel": "Environment",
+            "content": environment_name
+          }
         },
         {
-            "title": "Requests Executed",
-            "value": `${requests.length}`,
-            "short": true
+          "keyValue": {
+            "topLabel": "Location",
+            "content": `${region}: ${my_region_name}`
+          }
         },
         {
-            "title": "Environment",
-            "value": environment_name,
-            "short": true
+          "keyValue": {
+            "topLabel": "RequestsExecuted",
+            "content": `${requests.length}`
+          }
         },
         {
-            "title": "Assertions Passed",
-            "value": `${assertionsPassed} of ${assertionsTotal}`,
-            "short": true
+          "keyValue": {
+            "topLabel": "AssertionsPassed",
+            "content": `${assertionsPassed} of ${assertionsTotal}`
+          }
         },
         {
-            "title": "Location",
-            "value": location,
-            "short": true
-        },
-        {
-            "title": "Scripts Passed",
-            "value": `${scriptsPassed} of ${scriptsTotal}`,
-            "short": true
-        },
-        {
-            "title": "Total Response Time",
-            "value": `${totalResponseTime}ms`,
-            "short": true
+          "keyValue": {
+            "topLabel": "ScriptsPassed",
+            "content": `${scriptsPassed} of ${scriptsTotal}`
+          }
+        }, {
+          "keyValue": {
+            "topLabel": "TotalResponseTime",
+            "content": `${totalResponseTime}`
+          }
         }
-      ]
-    }
-  ];
-  
-  //add custom message 
-  if (isCustomMessage) {
-    gchatdata[0].fields.unshift(extraGchatMessage);
+        ]
+      }
+    ]
   }
+  ];
 
-  let gchatBody = {'attachments': gchatdata};
-  function postGchat(endpointUrl,gchatBody) {
+  function postGchat(endpointUrl, gchatBody) {
     axios.defaults.headers.common['Content-Type'] = 'application/json';
-    return axios.post(endpointUrl,gchatBody);
+    return axios.post(endpointUrl, gchatBody);
   }
 
   let testBody = {
-    'text': test_name
+    'cards': cards
   };
 
   //post message to gchat
 
-  if (!agent_expired||!hasAgentChannel) {
+  if (!agent_expired || !hasAgentChannel) {
     //test did not have an expired agent or there is no separate channel
     try {
-      let gchatPost = await postGchat(gchatUrl,testBody);
+      let gchatPost = await postGchat(gchatUrl, testBody);
       console.log(`Gchat Response Code: ${gchatPost.status}`);
-    } catch(e) {
+    } catch (e) {
       console.warn(e);
     }
   } else {
     //agent has failed and we have a custom channel to notify
     console.log(`Agent expired for Test run ${test_run_url}`);
     try {
-      let gchatPost = await postGchat(gchatAgentUrl,gchatBody);
+      let gchatPost = await postGchat(gchatAgentUrl, gchatBody);
       console.log(`Gchat Response Code: ${gchatPost.status}`);
-    } catch(e) {
+    } catch (e) {
       console.warn(e);
-    }    
-        
     }
+
+  }
 };
